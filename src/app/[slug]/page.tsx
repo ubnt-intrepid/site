de8da45469f617b7153c89dff9f8983a6854a3e7
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2024-11-30 13:59:13 +0900

    use rehype-class-names to apply article styles

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,39 +1,75 @@
 import { Metadata } from 'next'
 import Link from 'next/link'
 import React, { Fragment } from 'react'
-import { jsx, jsxs } from 'react/jsx-runtime'
-import { codeToHast } from 'shiki'
+import * as prod from 'react/jsx-runtime'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkDirective from 'remark-directive'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import remarkRehype from 'remark-rehype'
+import rehypeRaw from 'rehype-raw'
+import rehypeKatex from 'rehype-katex'
+import rehypeClassNames from 'rehype-class-names'
+import rehypeReact, { Components } from 'rehype-react'
+import { Code } from 'mdast'
+import { Handlers, State } from 'mdast-util-to-hast'
+import { Element } from 'hast'
+import { h } from 'hastscript'
 import { toJsxRuntime } from 'hast-util-to-jsx-runtime'
+import { visit } from 'unist-util-visit'
+import { codeToHast } from 'shiki'
+
 import FormattedDate from '@/components/FormattedDate'
 import Headline from '@/components/Headline'
 import Comments from '@/components/Comments'
 import { Calendar, Folder, Tag, Edit } from '@/components/MaterialIcon'
 import { siteRepoUrl } from '@/config'
 import { getPosts } from '@/lib/api'
-import markdownToJsx from '@/lib/markdownToJsx'
 
-export type Params = {
-    slug: string
+const remarkCalloutDirectives = () => {
+    return (tree: any) => {
+        visit(tree, (node) => {
+            if (
+                node.type !== 'containerDirective' &&
+                node.type !== 'leafDirective' &&
+                node.type !== 'textDirective'
+            ) {
+                return
+            }
+
+            if (node.name !== 'callout') {
+                return
+            }
+
+            const data = node.data || (node.data = {})
+            const tagName = node.type === 'textDirective' ? 'span' : 'div'
+
+            const attributes = node.attributes || {}
+            const className = attributes.className || (attributes.className = [])
+            className.push('bg-orange-50 px-5 py-3 my-10 rounded relative')
+
+            data.hName = tagName
+            data.hProperties = h(tagName, attributes).properties
+        })
+    }
 }
 
-export const generateStaticParams = async () => {
-    const posts = await getPosts()
-    return posts.map(({ slug }) => ({ slug })) satisfies Params[] as Params[]
-}
+const mdCodeBlockHandler = (state: State, node: Code) => {
+    let result: Element = {
+        type: 'element',
+        tagName: 'my-code-block',
+        properties: {
+            lang: node.lang,
+            title: node.meta,
+            content: node.value ? node.value + '\n' : '',
+        },
+        children: [],
+    }
+    state.patch(node, result)
+    result = state.applyData(node, result)
 
-export const generateMetadata = async ({ params }: { params: Promise<Params> }) => {
-    const { slug } = await params
-    const posts = await getPosts()
-    const { title } = posts.find(post => post.slug == slug) ?? {}
-    return {
-        title,
-    } satisfies Metadata
-}
-
-type CodeBlockProps = {
-    lang?: string
-    title?: string
-    content?: string
+    return result
 }
 
 const canonicalizeLanguageName = (lang?: string) => {
@@ -46,26 +82,113 @@ const canonicalizeLanguageName = (lang?: string) => {
     return lang
 }
 
-const CodeBlock: React.FC<CodeBlockProps> = async ({ lang, title, content }) => {
+const CodeBlock: React.FC<{
+    lang?: string
+    title?: string
+    content?: string
+}> = async ({ lang, title, content }) => {
     const hast = await codeToHast(content ?? '', {
         lang: canonicalizeLanguageName(lang),
-        theme: 'vitesse-light',
+        theme: 'vitesse-dark',
     })
     return toJsxRuntime(hast, {
         Fragment,
-        jsx,
-        jsxs,
+        jsx: prod.jsx,
+        jsxs: prod.jsxs,
         components: {
             pre: props => {
-                return (
-                    <div className='code-block'>
-                        { title ? <span className='title'>{title}</span> : null}
+                return title ? (
+                    <div className='my-6'>
+                        <span className='
+                            inline-block
+                            px-2
+                            py-1
+                            -mb-px
+                            rounded-t-sm
+                            text-sm
+                            font-mono
+                            font-bold
+                            bg-orange-600
+                            text-orange-50
+                        '>
+                            {title}
+                        </span>
+                        <pre {...props} className='mt-0' />
+                    </div>
+                ) : (
+                    <div>
                         <pre {...props} />
                     </div>
                 )
             }
         }
     })
+}
+
+const MarkdownArticle: React.FC<{
+    content?: string
+}> = async ({ content }) => {
+    const parsed = await unified()
+        .use(remarkParse, { fragment: true })
+        .use(remarkDirective)
+        .use(remarkCalloutDirectives)
+        .use(remarkGfm)
+        .use(remarkMath)
+        .use(remarkRehype, {
+            allowDangerousHtml: true,
+            handlers: { code: mdCodeBlockHandler } as Handlers
+        })
+        .use(rehypeRaw)
+        .use(rehypeKatex)
+        .use(rehypeClassNames, {
+            'p,ul,ol,pre,figure,blockquote': 'my-6',
+            'hr': 'flex mx-auto w-20',
+            'h1': 'text-3xl mt-5 mb-3',
+            'h2': 'text-2xl mt-5 mb-3',
+            'h3,h4,h5,h6': 'text-xl mt-5 mb-3',
+            'a': 'no-underline text-orange-600 hover:underline',
+            'ul,ol': 'list-outside pl-4',
+            'li': 'ml-6',
+            'ul': 'list-disc',
+            'ol': 'list-decimal',
+            'figure': 'text-center',
+            'img': 'block mx-auto',
+            'figcaption': 'text-sm',
+        })
+        .use(rehypeReact, {
+            Fragment: prod.Fragment,
+            jsx: prod.jsx,
+            jsxs: prod.jsxs,
+            components: {
+                'my-code-block': CodeBlock,
+            } as Partial<Components>,
+        })
+        .process(content)
+    return (
+        <article className='px-4 py-6'>
+            {parsed.result}
+        </article>
+    )
+}
+
+// ---
+
+export type Params = {
+    slug: string
+}
+
+export const generateStaticParams = async () => {
+    const posts = await getPosts()
+    return posts.map(({ slug }) => ({ slug })) satisfies Params[]
+}
+
+export const generateMetadata = async ({ params }: { params: Promise<Params> }) => {
+    const { slug } = await params
+    const posts = await getPosts()
+    const { title } = posts.find(post => post.slug == slug) ?? {}
+    return {
+        title,
+    } satisfies Metadata
 }
 
 const PostPage = async ({ params }: { params: Promise<Params> }) => {
@@ -79,7 +202,6 @@ const PostPage = async ({ params }: { params: Promise<Params> }) => {
     }
     const { title, date, tags, categories, content: rawContent, mdPath } = post
     const sourceUrl = `${siteRepoUrl}/blob/master/_posts/${mdPath}`;
-    const content = await markdownToJsx(rawContent ?? "", CodeBlock)
 
     return (
         <>
@@ -89,8 +211,8 @@ const PostPage = async ({ params }: { params: Promise<Params> }) => {
                 </p>
             </Headline>
 
-            <div className='container mx-auto'>
-                <div className='article-body'>{content}</div>
+            <div className='container mx-auto content-center'>
+                <MarkdownArticle content={rawContent} />
 
                 <div className='flex justify-between text-center text-sm'>
                     <span className='categories'>
