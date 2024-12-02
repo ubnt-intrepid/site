59354bcb3be94a0ad1443f5951b532de04c1f8d4
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2024-12-03 04:15:46 +0900

    wip: implement mdast -> JSX converters

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,15 +1,118 @@
 import React from 'react'
 import * as unified from 'unified'
+import mdast from 'mdast'
+import * as mdastMath from 'mdast-util-math'
 import remarkParse from 'remark-parse'
 import remarkDirective from 'remark-directive'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 import { inspect } from 'unist-util-inspect'
 
+declare module 'unified' {
+    interface CompileResultMap {
+        JsxResult: JsxResult
+    }
+}
+
+type JsxResult = {
+    raw: mdast.Node
+    jsx: string | React.JSX.Element
+}
 
 function remarkToJsx(this: unified.Processor) {
+    type CompilerState = {}
+
+    interface NodeTypeMap {
+        root: mdast.Root
+        text: mdast.Text
+        paragraph: mdast.Paragraph
+        heading: mdast.Heading
+        link: mdast.Link
+        inlineCode: mdast.InlineCode
+        code: mdast.Code
+        inlineMath: mdastMath.InlineMath
+        math: mdastMath.Math
+    }
+    type NodeType = keyof NodeTypeMap
+    type Emitter<N extends mdast.Node> = (state: CompilerState, node: N) => string | React.JSX.Element
+
+    const emitters: { [key in NodeType]: Emitter<NodeTypeMap[key]> } = {
+        root: (state, node) => {
+            return <div className='root'>
+                { node.children.map(child => emitOne(state, child)) }
+            </div>
+        },
+
+        text: (state, node) => {
+            return node.value
+        },
+
+        paragraph: (state, node) => {
+            return <p>
+                { node.children.map(child => emitOne(state, child)) }
+            </p>
+        },
+    
+        heading: (state, node) => {
+            return <div className='heading' x-depth={node.depth}>
+                { node.children.map(child => emitOne(state, child)) }
+            </div>
+        },
+    
+        link: (state, node) => {
+            return <span className='link' x-title={node.title} x-url={node.url}>
+                { node.children.map(child => emitOne(state, child)) }
+            </span>
+        },
+    
+        inlineCode: (state, node) => {
+            return <code>{node.value}</code>
+        },
+    
+        code: (state, node) => {
+            return <pre x-lang={node.lang} x-meta={node.meta}>
+                <code>
+                    {node.value}
+                </code>
+            </pre>
+        },
+    
+        inlineMath: (state, node) => {
+            return <code className='inline-math'>{node.value}</code>
+        },
+    
+        math: (state, node) => {
+            return <pre x-lang='math' x-meta={node.meta}>
+                <code>
+                    {node.value}
+                </code>
+            </pre>        
+        }    
+    }
+
+    const emitOne: Emitter<mdast.Node> = (state, node) => {
+        if (node.type in emitters) {
+            return emitters[node.type as NodeType](state, node as never)
+        }
+        return (
+            <details>
+                <summary className='bg-red-500'>unimplemented</summary>
+                <pre className='overflow-x-auto'>
+                    <code>
+                        {inspect(node, { color: false, showPositions: false })}
+                    </code>
+                </pre>
+            </details>
+        )    
+    }
+
     this.compiler = (tree, file) => {
-        return inspect(tree, { color: false, showPositions: false })
+        const state: CompilerState = {}
+        const compiled = emitOne(state, tree)
+        return {
+            raw: tree,
+            jsx: compiled
+        } satisfies JsxResult as JsxResult
     }
 }
 
@@ -27,13 +130,18 @@ const Markdown: React.FC<Props> = async ({ content }) => {
         .use(remarkMath)
         .use(remarkToJsx)
         .process(content)
+    const { raw, jsx } = parsed.result as JsxResult
     return (
         <article className='px-4 py-6'>
-            <pre className='overflow-x-auto'>
-                <code>
-                    {parsed.toString()}
-                </code>
-            </pre>
+            <details>
+                <summary className='bg-blue-200'>Raw MdAst</summary>
+                <pre className='overflow-x-auto'>
+                    <code>
+                        {inspect(raw, { color: false, showPositions: false })}
+                    </code>
+                </pre>
+            </details>
+            {jsx}
         </article>
     )
 }
